987f94fe85e6cc97dd22a857233a78b802b71d59
TOCOJI36 <62361858+TOCOJI36@users.noreply.github.com>
2020-03-20 06:11:46 +0300

    v3

--- untitled3/src/untitled3/core.clj
+++ untitled3/src/untitled3/core.clj
@@ -3,15 +3,15 @@
 
 (def queue (a/chan 100))
 
-(defn writing [chan n]
+(defn writing [chan_ n]
   (go (doseq [x (range 0 n) ]
       (let [value (rand-int 10)]
-        (>! chan value)))))
+        (>! chan_ value)))))
 
-(defn reading [chan]
+(defn reading [chan_]
   (go [] (while true
-           (loop [i (<! chan) v [] ]
-             (if (< 0 i) (recur (dec i) (conj v (<! chan)))
+           (loop [i (<! chan_) v [] ]
+             (if (< 0 i) (recur (dec i) (conj v (<! chan_)))
                          (println v))))))
 
 (defn -main [] (println "gogog"))
