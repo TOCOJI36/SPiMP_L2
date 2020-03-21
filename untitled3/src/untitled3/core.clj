993e4610304cebe7893e745aec762261abd440d8
TOCOJI36 <62361858+TOCOJI36@users.noreply.github.com>
2020-03-21 12:15:00 +0300

    4

--- untitled3/src/untitled3/core.clj
+++ untitled3/src/untitled3/core.clj
@@ -1,18 +1,21 @@
 (ns untitled3.core
-  (:require [clojure.core.async :refer [go <! >!] :as a]))
+  (:require [clojure.core.async :refer [go <! >! take!] :as a]))
 
-(def queue (a/chan 100))
+(def in_queue (a/chan 100))
+(def out_queue (a/chan 100))
 
 (defn writing [chan_ n]
   (go (doseq [x (range 0 n) ]
       (let [value (rand-int 10)]
         (>! chan_ value)))))
 
-(defn reading [chan_]
+(defn reading [input_chan output_chan]
   (go [] (while true
-           (loop [i (<! chan_) v [] ]
-             (if (< 0 i) (recur (dec i) (conj v (<! chan_)))
-                         (println v))))))
+           (loop [i (<! input_chan) v [] ]
+             (if (< 0 i) (recur (dec i) (conj v (<! input_chan)))
+                         (>! output_chan v))))))
+(defn read_chan [chan]
+  (take! chan (fn [r] (println "r->" r) (when (not= r nil) (read_chan chan)))))
 
 (defn -main [] (println "gogog"))
 
